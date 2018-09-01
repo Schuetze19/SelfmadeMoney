72f20e46026cdb32d75e51ae2cad5d55348d152f
Michael <cliption1995@gmail.com>
2018-09-02 00:00:59 +0200

    Simplen UnitTest hinzugefügt.

---
+++ app/src/test/java/com/example/dennis/selfmademoney/UserDaoTest.java
@@ -0,0 +1,18 @@
+package com.example.dennis.selfmademoney;
+
+import com.example.dennis.selfmademoney.dao.UserDao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserDaoTest {
+    private final UserDao userDao = new UserDao();
+
+    @Test
+    public void findUser(){
+        assertEquals(userDao.findAll().get(2),userDao.findById(3));
+        assertEquals(null,userDao.findById(11));
+    }
+
+}
