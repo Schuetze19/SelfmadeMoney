9bb7b7d96f83872e0802a1e7f2444f0cc11cd7ea
Michael <cliption1995@gmail.com>
2018-09-02 15:39:35 +0200

    User->MitgliedSeit hinzugefügt.

--- app/src/main/java/com/example/dennis/selfmademoney/ProfilActivity.java
+++ app/src/main/java/com/example/dennis/selfmademoney/ProfilActivity.java
@@ -3,7 +3,10 @@ package com.example.dennis.selfmademoney;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.dennis.selfmademoney.dao.UserDao;
+import com.example.dennis.selfmademoney.model.User;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -12,6 +15,7 @@ import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 public class ProfilActivity extends AppCompatActivity {
 
     private CircleImageView profilBild;
+    private TextView txtUsername, txtEmail, txtMitgliedSeit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class ProfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profil);
 
         profilBild = (CircleImageView) findViewById(R.id.profilBild);
+        txtUsername = (TextView) findViewById(R.id.txtUsername);
+        txtMitgliedSeit = (TextView) findViewById(R.id.txtMitgliedSeit);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+
+        User user = new UserDao().findById(1);
+        txtUsername.setText(user.getUsername());
+        txtMitgliedSeit.setText(user.getMitgliedSeit());
+        txtEmail.setText(user.getEmail());
 
         Picasso.get()
                 .load(R.drawable.ic_portrait_white_48dp)
--- app/src/main/java/com/example/dennis/selfmademoney/model/User.java
+++ app/src/main/java/com/example/dennis/selfmademoney/model/User.java
@@ -1,17 +1,23 @@
 package com.example.dennis.selfmademoney.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class User {
 
     private final long id;
     private final String username;
     private String password;
     private final String email;
+    private final Calendar mitgliedSeit = Calendar.getInstance();
 
     public User(final long id, final String username, final String email, String password){
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.mitgliedSeit.setTime(new Date());
     }
 
     public long getId() {
@@ -32,6 +38,10 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getMitgliedSeit(){
+        return new SimpleDateFormat("dd.MM.yyyy").format(mitgliedSeit.getTime());
     }
 
 }
