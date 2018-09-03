47041eeffbedfe993dcb7f39b44414a330dca1d0
Michael <cliption1995@gmail.com>
2018-09-03 19:15:39 +0200

    Profil Fragment angepasst. Main-Layout angepasst, sodass die Views nun ordnungsgemäß angezeigt werden.

--- app/src/main/java/com/example/dennis/selfmademoney/view/fragment/ProfilFragment.java
+++ app/src/main/java/com/example/dennis/selfmademoney/view/fragment/ProfilFragment.java
@@ -7,10 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dennis.selfmademoney.R;
+import com.example.dennis.selfmademoney.dao.UserDao;
+import com.example.dennis.selfmademoney.model.User;
+import com.example.dennis.selfmademoney.util.ClipboardUtil;
+import com.example.dennis.selfmademoney.view.activity.ProfilActivity;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 public class ProfilFragment extends Fragment {
+
+    private CircleImageView profilBild;
+    private TextView txtUsername, txtEmail, txtMitgliedSeit, txtFullName, txtPlace;
+    private EditText editDescription;
+    private ImageView emailToClip, usernameToClip;
 
     public ProfilFragment() {}
 
@@ -27,6 +44,46 @@ public class ProfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        profilBild = (CircleImageView) view.findViewById(R.id.profilBild);
+        txtUsername = (TextView) view.findViewById(R.id.txtUsername);
+        txtMitgliedSeit = (TextView) view.findViewById(R.id.txtMitgliedSeit);
+        txtEmail = (TextView) view.findViewById(R.id.txtEmail);
+        emailToClip = (ImageView) view.findViewById(R.id.emailToClip);
+        usernameToClip = (ImageView) view.findViewById(R.id.usernameToClip);
+        txtFullName = (TextView) view.findViewById(R.id.txtFullName);
+        txtPlace = (TextView) view.findViewById(R.id.txtPlace);
+        editDescription = (EditText) view.findViewById(R.id.editDescription);
+
+        final User user = new UserDao().findById(2);
+        txtUsername.setText(user.getUsername());
+        txtMitgliedSeit.setText(user.getMitgliedSeit());
+        txtEmail.setText(user.getEmail());
+        txtFullName.setText(user.getFullName() + " / " + user.getGeborenAm());
+        txtPlace.setText(user.getWohnort());
+        editDescription.setText(user.getBeschreibung());
+        emailToClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardUtil.copyToClipboard(getActivity(),"email", user.getEmail());
+                Toast.makeText(getContext(),user.getEmail()+" kopiert", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        usernameToClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardUtil.copyToClipboard(getActivity(),"username", user.getUsername());
+                Toast.makeText(getContext(),user.getUsername()+" kopiert", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Picasso.get()
+                .load(R.drawable.ic_portrait_white_48dp)
+                .placeholder(R.drawable.ic_portrait_white_48dp)
+                .error(R.drawable.ic_portrait_white_48dp)
+                .transform(new CropCircleTransformation())
+                .into(profilBild);
+        return view;
     }
 }
