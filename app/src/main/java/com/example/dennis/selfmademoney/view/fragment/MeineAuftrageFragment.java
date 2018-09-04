9994e23ca90610ac14092c327a24af0791af63d5
Michael <cliption1995@gmail.com>
2018-09-04 22:43:05 +0200

    Vorbereitungen für die arbeiten an "meine_auftrage" getroffen.

--- app/src/main/java/com/example/dennis/selfmademoney/view/fragment/MeineAuftrageFragment.java
+++ app/src/main/java/com/example/dennis/selfmademoney/view/fragment/MeineAuftrageFragment.java
@@ -2,6 +2,7 @@ package com.example.dennis.selfmademoney.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.view.ViewGroup;
 import com.example.dennis.selfmademoney.R;
 
 public class MeineAuftrageFragment extends Fragment {
+
+    private RecyclerView auftraege_recyclerview;
 
     public MeineAuftrageFragment() {}
 
@@ -21,7 +24,7 @@ public class MeineAuftrageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meine_auftrage, container, false);
-
+        auftraege_recyclerview = (RecyclerView) view.findViewById(R.id.auftraege_recyclerview);
         return view;
     }
 
