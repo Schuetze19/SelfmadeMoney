43b77b1eedb2d3c130235c396d2d3ea73db346ae
Michael <cliption1995@gmail.com>
2018-09-10 19:28:22 +0200

    Startseite zum Teil implementiert. Es fehlen die Userdaten (Der Auftrag weiß nicht, wer diesen erstellt hat).

---
+++ app/src/main/java/com/example/dennis/selfmademoney/adapter/StartseiteAuftraegeViewAdapter.java
@@ -0,0 +1,84 @@
+package com.example.dennis.selfmademoney.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.dennis.selfmademoney.R;
+import com.example.dennis.selfmademoney.model.Auftrag;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class StartseiteAuftraegeViewAdapter extends RecyclerView.Adapter<StartseiteAuftraegeViewAdapter.StartseiteAuftraegeViewHolder> {
+
+    private Context context;
+    private ArrayList<Auftrag> auftragList = new ArrayList<Auftrag>();
+
+    public StartseiteAuftraegeViewAdapter(@NonNull Context context, @NonNull ArrayList<Auftrag> auftragList){
+        this.context = context;
+        this.auftragList = auftragList;
+    }
+
+    @Override
+    public StartseiteAuftraegeViewAdapter.StartseiteAuftraegeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.startseite_auftraege_row_item, parent,false);
+        return new StartseiteAuftraegeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(StartseiteAuftraegeViewAdapter.StartseiteAuftraegeViewHolder holder, int position) {
+        final Auftrag auftrag = auftragList.get(position);
+        holder.txtUsername.setText("Username");
+        holder.txtErstellungsdatum.setText(new SimpleDateFormat("dd.MM.yyyy").format(auftrag.getErstellungsdatum()));
+        holder.txtBeschreibung.setText(auftrag.getBeschreibung());
+        holder.txtVerguetung.setText(String.format("%.2f €", auftrag.getVerguetung()));
+        holder.txtWohnort.setText("Wohnort");
+
+        holder.btnAnfrageStellen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        holder.btnIgnorieren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return auftragList.size();
+    }
+
+    public class StartseiteAuftraegeViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView txtUsername, txtErstellungsdatum, txtBeschreibung, txtVerguetung, txtWohnort;
+        public Button btnAnfrageStellen, btnIgnorieren;
+
+        public StartseiteAuftraegeViewHolder(View itemView) {
+            super(itemView);
+            txtUsername = itemView.findViewById(R.id.txtUsername);
+            txtErstellungsdatum = itemView.findViewById(R.id.txtErstellungsdatum);
+            txtBeschreibung = itemView.findViewById(R.id.txtBeschreibung);
+            txtVerguetung = itemView.findViewById(R.id.txtVerguetung);
+            txtWohnort = itemView.findViewById(R.id.txtWohnort);
+            btnAnfrageStellen = itemView.findViewById(R.id.btnAnfrageStellen);
+            btnIgnorieren = itemView.findViewById(R.id.btnIgnorieren);
+        }
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
--- app/src/main/java/com/example/dennis/selfmademoney/view/fragment/StartseiteFragment.java
+++ app/src/main/java/com/example/dennis/selfmademoney/view/fragment/StartseiteFragment.java
@@ -2,13 +2,22 @@ package com.example.dennis.selfmademoney.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dennis.selfmademoney.R;
+import com.example.dennis.selfmademoney.adapter.StartseiteAuftraegeViewAdapter;
+import com.example.dennis.selfmademoney.dao.AuftragDao;
 
 public class StartseiteFragment extends Fragment {
+
+    private RecyclerView startseite_auftraege;
+    private SearchView searchView;
+    private AuftragDao auftragDao = new AuftragDao();
 
     public StartseiteFragment() {}
 
@@ -25,6 +34,13 @@ public class StartseiteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_startseite, container, false);
+        View view = inflater.inflate(R.layout.fragment_startseite, container,false);
+        startseite_auftraege = view.findViewById(R.id.startseite_auftraege);
+        startseite_auftraege.setAdapter(new StartseiteAuftraegeViewAdapter(getActivity(), auftragDao.findAllLaufende()));
+        startseite_auftraege.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        searchView = view.findViewById(R.id.searchView);
+
+        return view;
     }
 }
